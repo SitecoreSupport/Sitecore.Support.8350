7a113e8abff79fd6b89fea27900f73fd1d1b61c3
Anton Kechashin <atk@sitecore.net>
2017-07-13 17:18:09 +0300

    Initial commit.

---
+++ src/Sitecore.Support.8350/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.8350")]
+[assembly: AssemblyProduct("Sitecore.Support.8350")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.8350/XA/Foundation/Multisite/Pipelines/HttpRequest/StoreSiteNameInCookie.cs
@@ -0,0 +1,15 @@
+﻿namespace Sitecore.Support.XA.Foundation.Multisite.Pipelines.HttpRequest
+{
+    using Sitecore.Pipelines.HttpRequest;
+
+    public class StoreSiteNameInCookie : Sitecore.XA.Foundation.Multisite.Pipelines.HttpRequest.StoreSiteNameInCookie
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            if (args.Context.Request.UrlReferrer == null || !args.Context.Request.UrlReferrer.Query.Contains("xmlcontrol=")) // don't change cookies if the request comes from an XML control
+            {
+                base.Process(args);
+            }
+        }
+    }
+}
